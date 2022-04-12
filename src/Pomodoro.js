5c03d7ac82dba0c1261e8e4ab691a51c9e6926a7
Raphael Kezzou <Raphael.Kezzou@live.fr>
2022-04-12 10:58:58 +0200

    Initial commit

---
+++ src/Pomodoro.js
@@ -0,0 +1,14 @@
+import React,{useState,useEffect} from 'react';
+import './Pomodoro.scss';
+//IMPORT COMPONENTS
+function Pomodoro() {
+  return (
+    <>
+    <header>
+        <h1>Pomodoro</h1>
+    </header>
+    </>
+  );
+}
+
+export default Pomodoro;
